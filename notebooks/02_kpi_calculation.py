5e48192dede59c7712fd6e22574a2efbbc688e46
Marcos  Borrillo <68borrismar@gmail.com>
2025-12-31 00:12:01 +0100

    Update 02_kpi_calculation.py

--- notebooks/02_kpi_calculation.py
+++ notebooks/02_kpi_calculation.py
@@ -11,15 +11,10 @@ df = pd.read_csv(MODEL_PATH)
 # Cálculo de KPIs
 df["Profit_Per_Voyage"] = df["Revenue_per_Voyage_USD"] - df["Operational_Cost_USD"]
 
-# KPI exploratorio tal como lo definiste
+# KPI exploratorio
 df["Res_Prof/Cost"] = df["Profit_Per_Voyage"] - df["Operational_Cost_USD"]
 
-# Eficiencia energética (si no viene ya agregada)
-# Si Efficiency_nm_per_kWh ya está a nivel viaje, no hace falta recalcular.
-# Aquí simplemente nos aseguramos de que exista.
-assert "Efficiency_nm_per_kWh" in df.columns, "Falta Efficiency_nm_per_kWh en el dataset"
-
-# Guardar dataset enriquecido con KPIs
+# Guarda el dataset enriquecido, con KPIs
 ENRICHED_PATH = DATA_DIR / "ship_performance_with_kpis.csv"
 df.to_csv(ENRICHED_PATH, index=False)
 print("Dataset con KPIs guardado en:", ENRICHED_PATH)
